12e5d6464000e873fc3ec2a85554971bcc32c386
sahiljain11 <sahilbot1@gmail.com>
2019-08-22 12:25:47 -0500

    Add tile hovering and README, reorganized code, and added multiple sprites to place on tile map

---
+++ README.md
@@ -0,0 +1,15 @@
+# Isometric Tile Map
+
+![Gameplay](isometricGameplay.gif)
+
+(Gameplay is much less laggy. gif creation software slows gamplay down significantly)
+
+## About
+
+In this project, I created a 3D tile map within a 2D engine. This included scaling the amount of tiles shown, inserting and deleting tiles, and using some interesting math to determine if the mouse/click is within a certain diamond in the tile map.
+
+## DragonRuby
+
+To create this game, I utilized the game engine DragonRuby. DragonRuby is an incredibly powerful game development engine that provides simplicity without taking away functionality. For more information, pleaes visit the following link:
+
+https://dragonruby.itch.io/dragonruby-gtk
--- app/main.rb
+++ app/main.rb
@@ -9,19 +9,21 @@ class Isometric
     end
 
     def defaults
-        state.quantity  ||= 6                              #Size of grid
-        state.tileSize  ||= [262 / 2, 194 / 2]             #width and heigth of orange tiles
-        state.tileGrid  ||= []                             #Holds ordering of tiles. Very left: 0,0. Very bottom: quantity-1, 0, etc
-        state.tileCords ||= []                             #Physical, rendering cordinates
-        state.initCords ||= [640 - (state.quantity / 2 * state.tileSize[0]), 330]    #Location of tile (0, 0)
-        state.sideSize  ||= [state.tileSize[0] / 2, 242 / 2]                         #Purple & green cube face size
-        state.mode      ||= :delete                                                  #Switches between :delete and :insert
-        state.spriteSelection ||= [['chess', 32, 30, 130 / 2, 216 / 2]]              #Storage for sprite information
-                                                                                     #['name', deltaX, deltaY, sizeW, sizeH]
-                                                                                     #^delta refers to distance from tile cordinates
-        state.currentSprite   ||= 0      #sprite currently selected
+        state.quantity              ||= 6                                                        #Size of grid
+        state.tileSize              ||= [262 / 2, 194 / 2]                                       #width and heigth of orange tiles
+        state.tileGrid              ||= []                                                       #Holds ordering of tiles
+        state.currentSpriteLocation ||= -1                                                       #Current Sprite hovering location 
+        state.tileCords             ||= []                                                       #Physical, rendering cordinates
+        state.initCords             ||= [640 - (state.quantity / 2 * state.tileSize[0]), 330]    #Location of tile (0, 0)
+        state.sideSize              ||= [state.tileSize[0] / 2, 242 / 2]                         #Purple & green cube face size
+        state.mode                  ||= :delete                                                  #Switches between :delete and :insert
+        state.spriteSelection       ||= [['river',    0, 0, 262 / 2, 194 / 2],
+                                         ['mountain', 0, 0, 262 / 2, 245 / 2],
+                                         ['ocean',    0, 0, 262 / 2, 194 / 2]]             #Storage for sprite information
+                                                                                           #['name', deltaX, deltaY, sizeW, sizeH]
+                                                                                           #^delta refers to distance from tile cords
 
-        #Orders tiles based on tile placement and fancy math
+        #Orders tiles based on tile placement and fancy math. Very left: 0,0. Very bottom: quantity-1, 0, etc
         if state.tileGrid == []
             tempX = 0
             tempY = 0
@@ -61,8 +63,19 @@ class Isometric
     end
 
     def render
-        outputs.solids << [0, 0, 1280, 720, 0, 0, 0]   #Background color
+        renderBackground
+        renderLeft
+        renderRight
+        renderTiles
+        renderObjects
+        renderLabels
+    end
 
+    def renderBackground
+        outputs.solids << [0, 0, 1280, 720, 0, 0, 0]   #Background color
+    end
+
+    def renderLeft
         #Shows the pink left cube face
         outputs.sprites << state.tileCords.map do
             |val|
@@ -71,7 +84,9 @@ class Isometric
                 state.sideSize[1], 'sprites/leftSide.png']
             end
         end
+    end
 
+    def renderRight
         #Shows the green right cube face
         outputs.sprites << state.tileCords.map do
             |val|
@@ -80,15 +95,23 @@ class Isometric
                 state.sideSize[1], 'sprites/rightSide.png']
             end
         end
+    end
 
+    def renderTiles
         #Shows the tile itself. Important that it's rendered after the two above!
         outputs.sprites << state.tileCords.map do
             |val|
             if val[2] == true     #Chcekcs if tile needs to be rendered
+              if val[5] == state.currentSpriteLocation
+                [val[0], val[1], state.tileSize[0], state.tileSize[1], 'sprites/selectedTile.png']
+              else
                 [val[0], val[1], state.tileSize[0], state.tileSize[1], 'sprites/tile.png']
+              end
             end
         end
+    end
 
+    def renderObjects
         #Renders the sprites on top of the tiles. Order of rendering: top corner to right corner and cascade down until left corner
         #to bottom corner.
         a = (state.quantity * state.quantity) - state.quantity
@@ -106,17 +129,53 @@ class Isometric
             iter = 0                if iter == state.quantity
             break if a < 0
         end
+    end
 
+    def renderLabels
         #Labels
         outputs.labels << [50, 680, 'Click to delete!',             5, 0, 255, 255, 255, 255] if state.mode == :delete
         outputs.labels << [50, 640, 'Press \'i\' for insert mode!', 5, 0, 255, 255, 255, 255] if state.mode == :delete
         outputs.labels << [50, 680, 'Click to insert!',             5, 0, 255, 255, 255, 255] if state.mode == :insert
         outputs.labels << [50, 640, 'Press \'d\' for delete mode!', 5, 0, 255, 255, 255, 255] if state.mode == :insert
-
     end
 
     def calc
-        
+        calcCurrentHover
+    end
+
+    def calcCurrentHover
+        #This determines what tile the mouse is hovering (or last hovering) over
+        x = inputs.mouse.position.x
+        y = inputs.mouse.position.y
+        m = (state.tileSize[1] / state.tileSize[0])   #slope
+        state.tileCords.map do
+            |val|
+            #Conditions that makes runtime faster. Checks if the mouse click was between tile dimensions (rectangle collision)
+            next unless val[0] < x && x < val[0] + state.tileSize[0]
+            next unless val[1] < y && y < val[1] + state.tileSize[1]
+            next unless val[2] == true
+            tempBool = false
+            if x == val[0] + (state.tileSize[0] / 2)
+                #The height of a diamond is the height of the diamond, so if x equals that exact point, it must be inside the diamond
+                tempBool = true
+            elsif x < state.tileSize[0] / 2 + val[0]
+                #Uses y = (m) * (x - x1) + y1 to determine the y values for the two diamond lines on the left half of diamond
+                tempY1 =      (m * (x - val[0])) + val[1] + (state.tileSize[1] / 2)
+                tempY2 = (-1 * m * (x - val[0])) + val[1] + (state.tileSize[1] / 2)
+                #Checks to see if the mouse click y value is between those temp y values
+                tempBool = true if y < tempY1 && y > tempY2
+            elsif x > state.tileSize[0] / 2 + val[0]
+                #Uses y = (m) * (x - x1) + y1 to determine the y values for the two diamond lines on the right half of diamond
+                tempY1 =      (m * (x - val[0] - (state.tileSize[0] / 2))) + val[1]
+                tempY2 = (-1 * m * (x - val[0] - (state.tileSize[0] / 2))) + val[1] + state.tileSize[1]
+                #Checks to see if the mouse click y value is between those temp y values
+                tempBool = true if y > tempY1 && y < tempY2
+            end
+
+            if tempBool == true
+                state.currentSpriteLocation = val[5]         #Current sprite location set to the order value
+            end
+        end
     end
 
     def process_inputs
@@ -124,7 +183,11 @@ class Isometric
         if inputs.keyboard.key_up.r
             $dragon.reset
         end
-        
+        checkTileSelected
+        switchModes
+    end
+
+    def checkTileSelected
         if inputs.mouse.down
             x = inputs.mouse.down.point.x
             y = inputs.mouse.down.point.y
@@ -136,7 +199,6 @@ class Isometric
                 next unless val[1] < y && y < val[1] + state.tileSize[1]
                 next unless val[2] == true
                 tempBool = false
-
                 if x == val[0] + (state.tileSize[0] / 2)
                     #The height of a diamond is the height of the diamond, so if x equals that exact point, it must be inside the diamond
                     tempBool = true
@@ -169,12 +231,14 @@ class Isometric
                         end
                     elsif state.mode == :insert
                         #adds the current sprite value selected to tileCords. (changes from the -1 earlier)
-                        val[6] = state.currentSprite
+                        val[6] = rand(state.spriteSelection.length)
                     end
                 end
             end
         end
+    end
 
+    def switchModes
         #Switches between insert and delete modes
         if inputs.keyboard.key_up.i && state.mode == :delete
             state.mode = :insert
